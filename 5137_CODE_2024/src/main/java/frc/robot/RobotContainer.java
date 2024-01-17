e9fcf11bfb3623f2653f71be479afcfad54c2531
JosephSwearingen <jhsweargn@gmail.com>
2024-01-16 16:34:27 -0800

    Fresh start

--- 5137_CODE_2024/src/main/java/frc/robot/RobotContainer.java
+++ 5137_CODE_2024/src/main/java/frc/robot/RobotContainer.java
@@ -8,7 +8,6 @@ import frc.robot.Commands.*;
 import frc.robot.Constants.*;
 import frc.robot.Subsystems.*;
 
-
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandPS4Controller;
@@ -18,43 +17,15 @@ public class RobotContainer {
   public static CommandPS4Controller driver;
   public static CommandPS4Controller operator;
 
-  public static Arm arm;
-
-  public static Arm_Commands arm_Commands;
-
   public RobotContainer() {
 
     driver = new CommandPS4Controller(0);
     operator = new CommandPS4Controller(1);
-    arm = new Arm();
-
-    arm_Commands = new Arm_Commands(arm);
 
     configureBindings();
   }
 
-  private void configureBindings() {
-    // Operator Bindings
-    operator.square()
-    .onTrue(arm_Commands.moveToIntake());
-
-    operator.triangle()
-    .onTrue(arm_Commands.moveToStart());
-
-    operator.circle()
-    .onTrue(arm_Commands.moveToAmp());
-
-    operator.cross()
-    .onTrue(arm_Commands.moveToSpeaker());
-
-    operator.L2()
-    .onTrue(arm_Commands.moveBackward())
-    .onFalse(arm_Commands.stopMoving());
-
-    operator.R2()
-    .onTrue(arm_Commands.moveForward())
-    .onFalse(arm_Commands.stopMoving());
-  }
+  private void configureBindings() {}
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
