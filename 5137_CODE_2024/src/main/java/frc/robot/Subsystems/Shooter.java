5060c4a09b72622a5652437148de728f5549bba4
jdelpego <jdelpego2@gmail.com>
2024-02-01 16:03:32 -0800

    Added shooter CAN IDs

--- 5137_CODE_2024/src/main/java/frc/robot/Subsystems/Shooter.java
+++ 5137_CODE_2024/src/main/java/frc/robot/Subsystems/Shooter.java
@@ -7,8 +7,8 @@ import com.revrobotics.CANSparkLowLevel.MotorType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Shooter extends SubsystemBase {
-private CANSparkMax lowerMotor = new CANSparkMax(5, MotorType.kBrushless);
-    private CANSparkMax higherMotor = new CANSparkMax(2, MotorType.kBrushless);
+private CANSparkMax lowerMotor = new CANSparkMax(14, MotorType.kBrushless);
+    private CANSparkMax higherMotor = new CANSparkMax(15, MotorType.kBrushless);
 
     public Shooter(){
         lowerMotor.setIdleMode(IdleMode.kCoast);
