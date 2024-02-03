84dba79f22e9ad62c6aae5afab8de80a0c3df28a
JosephSwearingen <jhsweargn@gmail.com>
2024-02-03 09:41:31 -0800

    Added files

---
+++ 5137_CODE_2024/src/main/java/frc/robot/Commands/LED_Commands.java
@@ -0,0 +1,5 @@
+package frc.robot.Commands;
+
+public class LED_Commands {
+    
+}
---
+++ 5137_CODE_2024/src/main/java/frc/robot/Subsystems/LED.java
@@ -0,0 +1,14 @@
+package frc.robot.Subsystems;
+
+import edu.wpi.first.wpilibj.AddressableLED;
+import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+
+public class LED {
+
+    private AddressableLED leds;
+    private AddressableLEDBuffer buffer;
+    
+    public LED() {
+        
+    }
+}
