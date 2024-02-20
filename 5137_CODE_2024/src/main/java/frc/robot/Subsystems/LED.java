2dd52e718d93960e960eb4ffcba2bd028bc93dea
JosephSwearingen <jhsweargn@gmail.com>
2024-02-20 13:09:21 -0800

    cool leds

--- 5137_CODE_2024/src/main/java/frc/robot/Subsystems/LED.java
+++ 5137_CODE_2024/src/main/java/frc/robot/Subsystems/LED.java
@@ -2,10 +2,13 @@ package frc.robot.Subsystems;
 
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.RobotState;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Robot;
 
 public class LED extends SubsystemBase {
 
@@ -24,7 +27,7 @@ public class LED extends SubsystemBase {
         length = buffer.getLength();
         leds.start();
         timer = new Timer();
-        timer.reset();
+        timer.restart();
     }
 
     public void solidColor(Color color, int brightness) {
@@ -39,7 +42,7 @@ public class LED extends SubsystemBase {
 
     public void CGChasing() {
         for (int i = 0; i < length; i++) {
-            var x = Math.floor(i%24);
+            var x = (double) i%24;
             if (x < 12) {
                 buffer.setRGB((i+(int)Math.floor(offset))%length, (int)((x/11)*255), 0, 0);
             } else {
@@ -48,7 +51,39 @@ public class LED extends SubsystemBase {
         }
         leds.setData(buffer);
 
+        offset+=0.3;
+    }
+
+    public void AllianceColorChasingUp() {
+        for (int i = 0; i < length; i++) {
+            var x = (double) i%12;
+            if (DriverStation.getAlliance().equals(Alliance.Red) || Robot.isSimulation()) {
+                buffer.setRGB((i+(int)Math.floor(offset))%length, (int)((x/11)*255), 0, 0);
+            } else if (DriverStation.getAlliance().equals(Alliance.Blue)) {
+                buffer.setRGB((i+(int)Math.floor(offset))%length, 0, 0, (int)((x/11)*255));
+            } else {
+                buffer.setRGB((i+(int)Math.floor(offset))%length, (int)((x/11)*255), (int)((x/11)*90), 0);
+            }
+        }
+        leds.setData(buffer);
+
         offset+=0.5;
+    }
+
+    public void AllianceColorChasingDown() {
+        for (int i = 0; i < length; i++) {
+            var x = (double) -i%-12;
+            if (DriverStation.getAlliance().equals(Alliance.Red) || Robot.isSimulation()) {
+                buffer.setRGB((i+(int)Math.floor(offset))%length, (int)((x/11)*255), 0, 0);
+            } else if (DriverStation.getAlliance().equals(Alliance.Blue)) {
+                buffer.setRGB((i+(int)Math.floor(offset))%length, 0, 0, (int)((x/11)*255));
+            } else {
+                buffer.setRGB((i+(int)Math.floor(offset))%length, (int)((x/11)*255), (int)((x/11)*90), 0);
+            }
+        }
+        leds.setData(buffer);
+
+        offset+=(length-0.5);
     }
 
     @Override
@@ -57,16 +92,16 @@ public class LED extends SubsystemBase {
             CGChasing();
         } else {
 
+
             if (RobotState.isAutonomous()) {
-                
+                AllianceColorChasingDown();
             }
 
             if (RobotState.isTeleop()) {
-
+                AllianceColorChasingUp();
             }
 
             if (RobotState.isTest()) {
-                timer.restart();
                 if (timer.hasElapsed(2)) {
                     timer.restart();
                 } else if (timer.hasElapsed(1)) {
@@ -76,5 +111,6 @@ public class LED extends SubsystemBase {
                 }
             }
         }
+        System.out.println(offset);
     }
 }
