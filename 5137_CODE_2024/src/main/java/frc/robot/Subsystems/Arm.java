fbd570cbdf01c677cefab1433dd157050b403e52
jdelpego <jdelpego2@gmail.com>
2024-03-11 19:53:42 -0700

    arm align

--- 5137_CODE_2024/src/main/java/frc/robot/Subsystems/Arm.java
+++ 5137_CODE_2024/src/main/java/frc/robot/Subsystems/Arm.java
@@ -163,7 +163,7 @@ public class Arm extends ProfiledPIDSubsystem {
     }
 
     public void alignToSpeaker(double position) {
-        setGoal((-align.calculateAngle(position))+(Math.PI/3)+Math.toRadians(1)); 
+        setGoal((-align.calculateAngle(position))+(Math.PI/3)); 
     }
 
     public double getGoal() {
@@ -186,9 +186,6 @@ public class Arm extends ProfiledPIDSubsystem {
         if (encoder.isConnected()) {
             useOutput(super.m_controller.calculate(getMeasurement()), super.m_controller.getSetpoint());
         }
-
-        System.out.println("2.0 " + Math.toDegrees(align.calculateAngle(2.5)));
-        System.out.println("2.5 " + Math.toDegrees(align.calculateAngle(2.0)));
 
 
     }
