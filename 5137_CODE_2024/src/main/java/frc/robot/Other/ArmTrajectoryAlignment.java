ac1901d06fb8127e03f9ef7f8b41bb4e7b597568
jdelpego <jdelpego2@gmail.com>
2024-03-26 00:34:21 -0700

    Shooter angle

--- 5137_CODE_2024/src/main/java/frc/robot/Other/ArmTrajectoryAlignment.java
+++ 5137_CODE_2024/src/main/java/frc/robot/Other/ArmTrajectoryAlignment.java
@@ -83,8 +83,8 @@ public class ArmTrajectoryAlignment {
 
     public double calculateAngle(double position) {
         if (position > minRange && position < maxRange) {
-            if (position >= 1.25 && position <= 4.75){
-                return Math.toRadians(-3.3015*(Math.pow(position, 2)) + 28.658*position-15.867);
+            if (position >= 1.25){
+                return Math.toRadians(0.5568*Math.pow(position, 3) - 8.2044*Math.pow(position,2) + 41.579*position - 25.952);
             }
             else{    
                 double x = position;
